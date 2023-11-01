b20fa9d04e10f972d0e1c0de16ec5dfeb29ab396
Adiletbaike <adiletdzhuraev@gmail.com>
2023-11-01 18:49:25 +0600

    map size

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -61,8 +61,8 @@ const Footer = () => {
             <h2 className="title-font font-medium text-white tracking-widest text-2xl mb-3">
               Адрес
             </h2>
-            <div className="flex xl:flex-nowrap md:flex-nowrap lg:flex-wrap flex-wrap justify-center items-end md:justify-start">
-              <div className="relative w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2">
+            <div className="flex xl:flex-nowrap md:flex-nowrap lg:flex-wrap flex-wrap items-end md:justify-start">
+              <div className="relative w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-0">
                 <iframe
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1462.1682663682736!2d74.61699718580707!3d42.8657405488866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x389eb7d2c40da377%3A0x9039cb257e332483!2sBismillah%20lodge!5e0!3m2!1sen!2skg!4v1697019266853!5m2!1sen!2skg"
                   width="auto"
